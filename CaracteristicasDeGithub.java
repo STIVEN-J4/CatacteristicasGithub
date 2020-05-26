863a80609159537cc6e339ff1d72ec9d4d945e6e
STIVEN-J4 <65369672+STIVEN-J4@users.noreply.github.com>
2020-05-26 09:59:47 -0500

    caracteristicas de github

---
+++ CaracteristicasDeGithub.java
@@ -0,0 +1,65 @@
+/*
+ * Implementar un algoritmo que le permita solicitar el ingreso
+por teclado las caracteristicas de Github
+ */
+package caracteristicasdegithub;
+
+/**
+ *
+ * @author Julio Jimenéz
+ */
+import java.util.Scanner;
+public class CaracteristicasDeGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Scanner objeto=new Scanner (System.in);
+        System.out.println("Ingrese la caracteristica 1 de Github:");
+        String caracteristica1=objeto.nextLine();
+
+        System.out.println("Ingrese la caracteristica 2 de Github:");
+        String caracteristica2=objeto.nextLine();
+
+        System.out.println("Ingrese la caracteristica 3 de Github:");
+        String caracteristica3=objeto.nextLine();
+
+        System.out.println("Ingrese la caracteristica 4 de Github:");
+        String caracteristica4=objeto.nextLine();
+
+        System.out.println("Ingrese la caracteristica 5 de Github:");
+        String caracteristica5=objeto.nextLine();
+
+        System.out.println("Ingrese la caracteristica 6 de Github:");
+        String caracteristica6=objeto.nextLine();
+
+        System.out.println("Ingrese la caracteristica 7 de Github:");
+        String caracteristica7=objeto.nextLine();
+
+        System.out.println("Ingrese la caracteristica 8 de Github:");
+        String caracteristica8=objeto.nextLine();
+
+        System.out.println("Ingrese la caracteristica 9 de Github:");
+        String caracteristica9=objeto.nextLine();
+
+        System.out.println("Ingrese la caracteristica 10 de Github:");
+        String caracteristica10=objeto.nextLine();
+
+        System.out.println("\n\n¿CUALES SON LAS CARACTERISTICAS DE GITHUB?:");
+
+         System.out.println("\n\nCaracteristica 1  de Github:" +caracteristica1);
+         System.out.println("\nCaracteristica 2  de Github:" +caracteristica2);
+         System.out.println("\nCaracteristica 3 de Github:" +caracteristica3);
+         System.out.println("\nCaracteristica 4 de Github:" +caracteristica4);
+         System.out.println("\nCaracteristica 5 de Github:" +caracteristica5);
+         System.out.println("\nCaracteristica 6 de Github:" +caracteristica6);
+         System.out.println("\nCaracteristica 7 de Github:" +caracteristica7);
+         System.out.println("\nCaracteristica 8 de Github:" +caracteristica8);
+         System.out.println("\nCaracteristica 9 de Github:" +caracteristica9);
+         System.out.println("\nCaracteristica 10 de Github:" +caracteristica10);
+    }
+
+}
